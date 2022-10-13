72064bb2eac2c620894613f13b62852cd9eb4a85
jayanthkrishna <gannerla16038@iiitd.ac.in>
2022-10-13 23:35:31 +0530

    Added rate limiting

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -1,10 +1,33 @@
 package helpers
 
+import (
+	"os"
+	"strings"
+)
+
 func RemoveDomainError(url string) bool {
-	return false
+
+	if url == os.Getenv("DOMAIN") {
+		return false
+	}
+
+	newURL := strings.Replace(url, "http://", "", 1)
+
+	newURL = strings.Replace(newURL, "https://", "", 1)
+
+	newURL = strings.Replace(newURL, "www.", "", 1)
+	newURL = strings.Split(newURL, "/")[0]
+
+	if newURL == os.Getenv("DOMAIN") {
+		return false
+	}
+
+	return true
 }
 
-func EnforceHttps(url string) string {
-
+func EnforceHttp(url string) string {
+	if url[:4] != "http" {
+		return "http://" + url
+	}
 	return url
 }
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jayanthkrishna/url-shortening-service/database"
 	"github.com/jayanthkrishna/url-shortening-service/helpers"
 )
 
@@ -34,6 +38,30 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//implement rate limiting
 
+	r2 := database.CreateClient(1)
+
+	defer r2.Close()
+
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+
+	if err == redis.Nil {
+
+		r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second)
+
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valINT, _ := strconv.Atoi(val)
+
+		if valINT <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "rate limit Exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Nanosecond,
+			})
+		}
+
+	}
+
 	//check if the url is an actual URL
 
 	if !govalidator.IsURL(body.URL) {
@@ -52,6 +80,9 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//enforce https
 
-	body.URL = helpers.EnforceHttps(body.URL)
+	body.URL = helpers.EnforceHttp(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
+
 	return nil
 }
